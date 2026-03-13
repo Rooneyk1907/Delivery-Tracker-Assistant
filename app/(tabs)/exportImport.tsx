8e7bda59b114b7f00f04a19ff0c50b397da5857b
Kevin Rooney <rooneyjkevin@proton.megit config --global user.name Rooneyk1907>
2026-03-12 19:35:53 -0500

    created exportImport page. added page to tab selection

---
+++ app/(tabs)/exportImport.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function exportImport() {
+	return (
+		<View>
+			<Text>Export and Import Page</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
